a5dac2bcd444bc5a3f540ed216ac9014ea89a37f
mzamoranoc <mzamoran@chicagobooth.edu>
2024-10-23 21:58:53 +0000

    various

--- app.rb
+++ app.rb
@@ -1,44 +1,60 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# Redirect root to addition form
 get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+# Route for displaying the addition form
+get("/add") do
   erb(:add_form)
 end
 
+# Route for processing the addition form
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
-
+  @second_num = params.fetch("second_num").to_f
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
+# Route for displaying the subtraction form
 get("/subtract") do
   erb(:sub_form)
 end
 
+# Route for processing the subtraction form
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
-
+  @result = @first_num - @second_num
   erb(:sub_result)
 end
 
+# Route for displaying the multiplication form
 get("/multiply") do
   erb(:multiplication_form)
 end
 
-get("/wizard multiply") do
+# Route for processing the multiplication form
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
+# Route for displaying the division form
 get("/divide") do
   erb(:div_form)
 end
 
+# Route for processing the division form
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num / @second_num
   erb(:div_result)
 end
+
